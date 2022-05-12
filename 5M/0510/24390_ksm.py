5b10e2215861fec6670fd2b2e0a140fefbb75161
김 성 민 <origene42@naver.com>
2022-05-12 20:16:14 +0900

    13335_ksm

--- 5M/0510/24390_ksm.py
+++ 5M/0510/24390_ksm.py
@@ -1,3 +1,46 @@
+# def jori(n):
+
+#     if n == 0:
+#         return 0
+
+
+#     visited = [0]*(time+1)
+
+#     Q = [n]
+#     visited[n] = 1
+
+#     while n > 0:
+#         n = Q.pop(0)
+
+#         for j in [60,6,3,1]:
+#             Nn = n - j
+
+#             if Nn == 0:
+#                 return visited[n]
+            
+#             if 0 <= Nn  and visited[Nn] == 0:
+#                 visited[Nn] = visited[n]+1
+#                 Q.append(Nn)
+            
+    
+
+# N,M = map(int,input().split(':'))
+
+# time = N*6 + M//10
+
+# if time <3:
+#     print(time+1)
+
+# elif time % 60 < 3:
+#     print(time//60 + 1 + (time % 60))
+   
+# else:
+#     cnt = 1
+#     joritime = time - 3
+#     cnt += jori(joritime)
+
+#     print(cnt)
+
 def jori(n):
 
     if n == 0:
---
+++ 5M/0512/13335_ksm.py
@@ -0,0 +1,23 @@
+from collections import deque
+n,w,L = map(int,input().split()) # n -> 다리를 건너는 트럭수 w 다리길이  L 최대하중
+
+truckList = list(map(int,input().split()))
+truckNum = [0]*n
+
+bridge = deque([0]*w)
+
+
+cnt = 0
+while truckList:
+    cnt +=1
+    bridge.popleft()
+    if sum(bridge) + truckList[0] <= L:
+        bridge.append(truckList[0])
+        truckList.pop(0)
+
+    else: 
+        bridge.append(0)
+
+cnt += w
+
+print(cnt)
